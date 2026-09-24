ddc7bb3b6a4689455d729c35d0fb476eb46ca1d4
Anton <anton-qz@yandex.ru>
2026-09-24 15:40:55 +0700

    logging tests

--- src/test/java/ru/ivanov/TracedTest.java
+++ src/test/java/ru/ivanov/TracedTest.java
@@ -2,12 +2,16 @@ package ru.ivanov;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.ivanov.tools.AllureTraceExtension;
 import ru.ivanov.tools.HttpStepClient;
 import ru.ivanov.tools.TheInternetSpec;
 
 @ExtendWith(AllureTraceExtension.class)
 public class TracedTest {
+    private static final Logger log = LoggerFactory.getLogger(TracedTest.class);
+
     @Test
     void testPositiveHttpRequest() {
         HttpStepClient.givenWithTrace()
@@ -20,6 +24,7 @@ public class TracedTest {
 
     @Test
     void testNegativeDistributionFailureReport() {
+        log.info("Запуск теста который должен упасть, лог должен сохранится в Allure отчете");
         HttpStepClient.givenWithTrace()
                 .spec(TheInternetSpec.getFailSpec())
                 .when()
